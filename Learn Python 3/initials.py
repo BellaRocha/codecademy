21ae0577156c99c1132f8a8653257947e8237455
BellaRocha <bellailr@bu.edu>
2021-10-21 14:46:00 -0400

    Create initials.py

---
+++ Learn Python 3/initials.py
@@ -0,0 +1,21 @@
+# PROJECT: BLOCK LETTERS
+
+# ASCII art: graphic design technique that consists of pictures pieced together from individual characters.
+
+# Write a Python program called initials.py that displays the initials of your name in block letters as shown and dip your toes into ASCII art.
+
+# First Name: Isabella
+# Last Name: Rocha
+# Fun Fact: I hate pickles
+
+print(""" 
+
+IIIII   RRRR
+  I     R   R
+  I     R   R
+  I     RRRR
+  I     R R
+  I     R  R
+IIIII   R   R
+
+""")
