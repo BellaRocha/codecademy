ab04447dac64c3a2df4bae395dbf95864ea80189
Isabella Rocha <ilr.human@gmail.com>
2022-06-17 19:28:04 -0400

    Create daily-buzz.sh

---
+++ Front-End Engineer Career Path/daily-buzz.sh
@@ -0,0 +1,41 @@
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ ls
+culture  health  national  technology
+$ cd national/politics/
+$ ls
+policy
+$ mkdir elections
+$ cd elections/
+$ touch candidates.txt issues.txt
+$ cd ../../..
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ mkdir business
+$ cd business/
+$ ls
+$ mkdir startups
+$ cd startups
+$ cd ../
+$ mkdir startups/disruptors
+$ ls
+startups
+$ cd startups/
+$ ls
+disruptors
+$ cd ../
+$ pwd
+/home/ccuser/workspace/daily-buzz/business/startups
+$ cd ../
+$ ls
+startups
+$ touch startups/disruptors/tech.txt
+$ touch startups/disruptors/design.txt
+$ touch startups/disruptors/education.txt
+$ ls
+startups
+$ cd ../
+$ ls
+business  culture  health  national  technology
+$ pwd
+/home/ccuser/workspace/daily-buzz
