107169c88fbcda19253a8f273bb337d2f852c53f
Isabella Rocha <ilr.human@gmail.com>
2022-06-17 19:07:29 -0400

    Create bicycle-world.sh

---
+++ Front-End Engineer Career Path/bicycle-world.sh
@@ -0,0 +1,20 @@
+$ pwd
+/home/ccuser/workspace/bicycle-world-ii
+$ ls
+brands.txt  freight  mountain  racing
+$ cd freight
+$ ls
+messenger  porteur
+$ cd porteur
+$ cd ../..
+$ cd mountain/downhill/
+$ mkdir dirt.txt
+$ mkdir mud.txt
+$ ls
+dirt.txt  heavyweight  lightweight  mud.txt
+$ mkdir safety
+$ cd bicycle-world-ii/
+$ mkdir bmx
+$ touch bmx/tricks.txt
+$ ls
+bmx  brands.txt  freight  mountain  racing
